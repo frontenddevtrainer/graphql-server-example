902e43fa08a653dec711f4a2a15d0b10e1dc2858
Admin <admin@example.com>
2024-03-13 16:34:01 +0000

    Added query for user and resolve using json placeholder.

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,21 @@
+import axios from "axios";
+
 const resolvers = {
   Query: {
-    books: () => [{ title: "React Learning", author: "Developer" }, { title: "Angular Learning", author: "Developer" }],
+    books: () => [
+      { title: "React Learning", author: "Developer" },
+      { title: "React Learning", author: "Developer" },
+    ],
+    users: async () => {
+      try {
+        const response = await axios(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        return response.data;
+      } catch (error) {
+        return null;
+      }
+    },
   },
 };
 
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,12 +1,43 @@
 const typeDefs = `#graphql
 
+  type Company {
+    name: String
+    catchPhrase: String
+    bs: String
+  }
+
+  type Geo {
+    lat: String
+    lng: String
+  }
+
+  type Address {
+    street: String
+    suite: String
+    city: String
+    zipcode: String
+    geo: Geo
+  }
+
+  type User {
+    id: Int
+    name: String
+    username: String
+    email: String
+    phone: String
+    website: String
+    company: Company
+    address: Address
+  }
+
   type Book {
     title: String
     author: String
   }
 
   type Query {
-    books: [Book]
+    books: [Book],
+    users: [User]
   }
 
 `;
